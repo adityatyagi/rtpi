0695dec69e8ffb14ebc1d6ae7419c573182e1953
Parv Jain <parvjn616@gmail.com>
2017-09-13 10:59:34 +0530

    CU products to db with dynamic price history files

--- app/routes/flipkart.js
+++ app/routes/flipkart.js
@@ -2,6 +2,7 @@ var request = require('request'); // for creating http requests
 var cheerio = require('cheerio'); // for web scrapping
 var fs = require('fs'); // for the file system
 var moment = require('moment');
+var Product = require('../models/product');
 
 module.exports = function(app) {
 
@@ -11,30 +12,73 @@ module.exports = function(app) {
         request(url, function(error, response, body) {
             if (!error) {
                 var $ = cheerio.load(body);
-                current_time = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
                 var json = {
                     title: "",
                     current_price: "",
                     mrp: "",
-                    previous_prices: {
-                        price1: "",
-                        time1: ""
-                    }
+                    current_time: "",
+                    price_history_file: ""
                 };
 
                 json.title = $("[class='_3eAQiD']").text();
                 json.current_price = $("[class='_1vC4OE _37U4_g']").text().replace('₹', '');
                 json.mrp = $("[class='_3auQ3N _16fZeb']").text().replace('₹', '');
-                json.previous_prices.price1 = json.current_price;
-                json.previous_prices.time1 = current_time;
-            }
-            res.json(json);
+                var price_history_file = json.title.replace(/[^a-zA-Z0-9]/g, '');
+                json.price_history_file = price_history_file;
+                var current_time = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
+                json.current_time = current_time;
 
-            // writing the changes to the file and this file will then be processed in mail.js
-            fs.writeFile("app/data/productDetails.json", JSON.stringify(json, null, 4), function() {
-                console.log('Details successfully fetched!');
-            });
+                res.json(json);
+
+
+                //write product details to database
+                //check if product already exists
+                Product.findOne({ 'price_history_file' : price_history_file }, function(err, product) {
+                  if (err)
+                    console.log(err);
+                  //if product already exists in db
+                  if (product) {
+                    data = {
+                      time: json.current_time,
+                      price: json.current_price
+                    }
+                    // writing the changes to the file and this file will then be processed in mail.js
+                    fs.appendFile("app/data/"+price_history_file+".json", JSON.stringify(data, null, 4), function() {
+                        console.log('Details successfully appended to file %s.json',price_history_file);
+                    });
+
+                    // replace current price with latest price
+                    product.current_time = json.current_time;
+                    product.current_price = json.current_price;
+                    product.save(function(err) {
+                        if (err)
+                            throw err;
+                        else{
+                          console.log('Details saved to database');
+                        }
+                    });
+                  } else {
+                    var newProduct = new Product();
+                    newProduct.title = json.title;
+                    newProduct.current_price = json.current_price;
+                    newProduct.mrp = json.mrp;
+                    newProduct.price_history_file = json.price_history_file;
+                    newProduct.current_time = json.current_time;
+
+                    // writing the changes to the file and this file will then be processed in mail.js
+                    fs.appendFile("app/data/"+price_history_file+".json", JSON.stringify(json, null, 4), function() {
+                        console.log('Details successfully written to file %s.json',price_history_file);
+                    });
+                    newProduct.save(function(err) {
+                        if (err)
+                            throw err;
+                        else{
+                          console.log('Details saved to database');
+                        }
+                    });
+                  }
+                });
+            }
         });
     });
-
 }
